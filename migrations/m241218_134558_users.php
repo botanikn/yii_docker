d7646378e462d83e73c7db89a6ae5643d73fd3cc
yusef.alassar <yusef.alassar@simpleone.ru>
2024-12-18 17:31:14 +0300

    docker apache

---
+++ migrations/m241218_134558_users.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m241218_134558_users
+ */
+class m241218_134558_users extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m241218_134558_users cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m241218_134558_users cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
