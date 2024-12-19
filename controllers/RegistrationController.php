cd43218b2f4104d86cd7c977b0df0bbb93025f22
yusef.alassar <yusef.alassar@simpleone.ru>
2024-12-19 18:23:36 +0300

    model instance in controller and reg view

--- controllers/RegistrationController.php
+++ controllers/RegistrationController.php
@@ -2,12 +2,18 @@
 
 namespace app\controllers;
 use \yii\web\Controller;
+use Yii;
+use app\models\RegistrationForm;
 
 class RegistrationController extends Controller
 {
 
     public function actionIndex() {
-        return $this->render('registration');
+        if (!Yii::$app->user->isGuest) {
+            return $this->goHome();
+        }
+        $model = new RegistrationForm();
+        return $this->render('index', ['model' => $model]);
     }
 
 }
---
+++ views/registration/index.php
@@ -0,0 +1,4 @@
+<?php
+
+use yii\helpers\Html;
+use yii\bootstrap5\ActiveForm;
