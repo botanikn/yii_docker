a9b9d23466c64eb49cd26dfa81b038b3342779a0
yusef.alassar <yusef.alassar@simpleone.ru>
2024-12-19 18:00:48 +0300

    registration controller and model with validation

---
+++ controllers/RegistrationController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace app\controllers;
+use \yii\web\Controller;
+
+class RegistrationController extends Controller
+{
+
+    public function actionIndex() {
+        return $this->render('registration');
+    }
+
+}
---
+++ models/RegistrationForm.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace app\models;
+use yii\base\Model;
+
+class RegistrationForm extends Model {
+
+    public $login;
+    public $password;
+    public $firstName;
+    public $lastName;
+    public $email;
+
+    // Валидация параметров с формы
+    public function rules() {
+        return [[
+            ['login', 'password', 'firstName', 'lastName', 'email'], 'required'],
+            ['email', 'email'],
+            ['password', 'string', 'min' => 10]
+        ];
+    }
+
+}
