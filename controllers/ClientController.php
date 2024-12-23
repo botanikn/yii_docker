973e6aec85532ee2b6b9707acad3262b5e6d8d89
yusef.alassar <yusef.alassar@simpleone.ru>
2024-12-23 15:33:27 +0300

    hot fix to access

--- controllers/ClientController.php
+++ controllers/ClientController.php
@@ -5,6 +5,7 @@ namespace app\controllers;
 class ClientController extends UserController {
 
     public function actionIndex() {
+        $this->actionAppropUser(2);
         return $this->render('index');
     }
 
