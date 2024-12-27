097b358cfd8e63905218fc19a7864eabc3991f66
yusef.alassar <yusef.alassar@simpleone.ru>
2024-12-27 13:38:55 +0300

    little fix

--- config/web.php
+++ config/web.php
@@ -42,14 +42,15 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
+
         'urlManager' => [
-            'enablePrettyUrl' => true,
-            'showScriptName' => false,
+            'enablePrettyUrl' => false,
+            'showScriptName' => true,
             'rules' => [
+               '' => '',
             ],
         ],
-        */
+
     ],
     'homeUrl' => ['/registration/index'],
     'params' => $params,
